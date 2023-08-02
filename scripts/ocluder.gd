60bf0df4cea11ce558f33204e6341c4a39240dfe
UserDotCpp <78625020+UserDotCpp@users.noreply.github.com>
2023-08-02 20:49:46 -0300

    build: started migration to godot 4.1

--- scripts/ocluder.gd
+++
@@ -1,8 +0,0 @@
-extends CSGSphere
-
-func _ready():
-	pass # Replace with function body.
-
-# warning-ignore:unused_argument
-func _physics_process(delta):
-	self.global_translation = $Unit.global_translation
